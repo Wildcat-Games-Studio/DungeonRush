81a055c9a0c76bf60127b077742f139126a443b7
Cowman9002 <54774464+Cowman9002@users.noreply.github.com>
2022-03-03 10:22:25 -0500

    Code rework crystal boss
    First four states for the crystal boss have been turned into objects for easier expansions and working

--- Assets/Scripts/CrystalBoss.cs
+++ Assets/Scripts/CrystalBoss.cs
@@ -37,29 +37,47 @@ public class CrystalBoss : MonoBehaviour
     public LineRenderer attackLine;
     public SpikeGenerator spikeGenerator;
 
-    private Animator m_animator;
+    private Animator _animator;
 
-    [SerializeField]
-    private enum State { Idle, Charge, Hold, Collapse, Shoot, Recover, Teleport, TeleportWaiting, Spiking };
-    [SerializeField]
-    private State m_state;
+    private CrystalBossState _currentState;
+    public CrystalBossState CurrentState
+    {
+        get { return _currentState; }
+        set
+        {
 
-    private float m_timer;
+            _currentState?.Exit();
+            _currentState = value;
+            _currentState?.Enter();
 
-    private Vector2 m_hitPoint;
-    private float m_teleportWaitTime;
+        }
+    }
+    public CrystalBossIdle StateIdle { get; private set; }
+    public CrystalBossCharge StateCharge { get; private set; }
+    public CrystalBossHold StateHold { get; private set; }
+    public CrystalBossCollapse StateCollapse { get; private set; }
 
-    private float m_laserAngle;
-    private float m_laserTraceDir;
-    private uint m_numTeleports;
+    public float Timer { get; private set; }
+
+    private Vector2 _hitPoint;
+    private float _teleportWaitTime;
+
+    private float _laserAngle;
+    private float _laserTraceDir;
+    private uint _numTeleports;
 
     private void Awake()
     {
-        m_animator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
 
-        m_state = State.Idle;
-        m_timer = 0.0f;
-        m_numTeleports = 0;
+        Timer = 0.0f;
+        _numTeleports = 0;
+
+        StateIdle = new CrystalBossIdle(this, 1.0f);
+        StateCharge = new CrystalBossCharge(this, chargeTime, bubbleTransform, maxBubbleSize);
+        StateHold = new CrystalBossHold(this, holdTime, bubbleTransform, holdWobble, holdAmplitude);
+        StateCollapse = new CrystalBossCollapse(this, collapseTime, bubbleTransform);
+        CurrentState = StateIdle;
     }
 
     void Start()
@@ -67,221 +85,149 @@ public class CrystalBoss : MonoBehaviour
         targetLine.enabled = false;
         attackLine.enabled = false;
 
-        spikeGenerator.attackFinished = FinishSpike;
+        //spikeGenerator.attackFinished = FinishSpike;
     }
 
     void Update()
     {
-        switch (m_state)
-        {
-            case State.Idle: IdleState(); break;
-            case State.Charge: ChargeState(); break;
-            case State.Hold: HoldState(); break;
-            case State.Collapse: CollapseState(); break;
-            case State.Shoot: ShootState(); break;
-            case State.Recover: RecoverState(); break;
-            case State.TeleportWaiting: TeleportWaitState(); break;
-            default: break;
-        }
+        CurrentState.Update(Time.deltaTime);
     }
 
-    private void IdleState()
-    {
-        if (m_timer >= 1.0f)
-        {
-            m_timer = 0.0f;
-            ChangeStateCharge();
-            return;
-        }
+    public void TickTimer(float delta) => Timer += delta;
+    public void ResetTimer() => Timer = 0.0f;
+    public bool CheckTimer(float value) => Timer >= value;
 
-        m_timer += Time.deltaTime;
-    }
 
-    private void ChangeStateCharge()
-    {
-        m_state = State.Charge;
-        targetLine.enabled = true;
+    //private void CollapseState()
+    //{
+    //    if (Timer >= collapseTime)
+    //    {
+    //        Timer = 0.0f;
+    //        _state = State.Shoot;
 
-        targetLine.SetPositions(new Vector3[] { transform.position, transform.position });
-    }
+    //        bubbleTransform.localScale = Vector2.zero;
+    //        attackLine.SetPosition(1, _hitPoint);
 
-    private void ChargeState()
-    {
-        Vector2 dir2player = (GameState.Instance.player.transform.position - transform.position).normalized;
+    //        Vector2 dir2player = (GameState.Instance.player.transform.position - transform.position).normalized;
+    //        float playerAngle = Vector2.SignedAngle(Vector2.right, dir2player) * Mathf.Deg2Rad;
+    //        _laserTraceDir = Mathf.Sign(playerAngle - _laserAngle);
 
-        if (m_timer >= chargeTime)
-        {
-            m_timer = 0.0f;
-            m_state = State.Hold;
+    //        return;
+    //    }
 
-            bubbleTransform.localScale = new Vector2(maxBubbleSize, maxBubbleSize);
-            m_laserAngle = Vector2.SignedAngle(Vector2.right, dir2player) * Mathf.Deg2Rad;
+    //    Timer += Time.deltaTime;
 
-            return;
-        }
+    //    bubbleTransform.localScale = Vector2.Lerp(new Vector2(maxBubbleSize, maxBubbleSize), Vector2.zero, Timer / collapseTime);
+    //    attackLine.SetPosition(1, Vector2.Lerp(transform.position, _hitPoint, Timer / collapseTime));
 
-        m_timer += Time.deltaTime;
-        bubbleTransform.localScale = Vector2.Lerp(Vector2.zero, new Vector2(maxBubbleSize, maxBubbleSize), m_timer / chargeTime);
+    //}
 
-        Vector2? endpt = CalcLaserEnd(dir2player);
+    //private void ShootState()
+    //{
+    //    if (Timer >= shootTime)
+    //    {
+    //        Timer = 0.0f;
+    //        _state = State.Recover;
+    //        return;
+    //    }
 
-        if(endpt != null)
-        {
-            targetLine.SetPosition(1, endpt.Value);
-            m_hitPoint = endpt.Value;
-        }
-    }
+    //    Timer += Time.deltaTime;
 
-    private void HoldState()
-    {
-        if (m_timer >= holdTime)
-        {
-            m_timer = 0.0f;
-            m_state = State.Collapse;
+    //    _laserAngle += _laserTraceDir * Time.deltaTime * laserSpeed;
+    //    Vector2 dir = new Vector2();
+    //    dir.x = Mathf.Cos(_laserAngle);
+    //    dir.y = Mathf.Sin(_laserAngle);
 
-            attackLine.SetPosition(0, transform.position);
-            attackLine.SetPosition(1, transform.position);
-            targetLine.enabled = false;
-            attackLine.enabled = true;
+    //    Vector2? endpt = CalcLaserEnd(dir);
+    //    if (endpt != null)
+    //    {
+    //        attackLine.SetPosition(1, (Vector3)endpt);
+    //        _hitPoint = endpt.Value;
+    //    }
+    //}
 
-            return;
-        }
+    //private void RecoverState()
+    //{
+    //    if (Timer >= recoverTime)
+    //    {
+    //        Timer = 0.0f;
+    //        bubbleTransform.localScale = Vector2.zero;
+    //        attackLine.SetPosition(1, _hitPoint);
+    //        attackLine.enabled = false;
 
-        m_timer += Time.deltaTime;
-        float s = Mathf.Sin(m_timer * holdWobble) * holdAmplitude;
-        bubbleTransform.localScale = new Vector2(maxBubbleSize + s, maxBubbleSize + s);
-    }
+    //        ChangeStateTeleport();
 
-    private void CollapseState()
-    {
-        if (m_timer >= collapseTime)
-        {
-            m_timer = 0.0f;
-            m_state = State.Shoot;
+    //        return;
+    //    }
 
-            bubbleTransform.localScale = Vector2.zero;
-            attackLine.SetPosition(1, m_hitPoint);
+    //    Timer += Time.deltaTime;
 
-            Vector2 dir2player = (GameState.Instance.player.transform.position - transform.position).normalized;
-            float playerAngle = Vector2.SignedAngle(Vector2.right, dir2player) * Mathf.Deg2Rad;
-            m_laserTraceDir = Mathf.Sign(playerAngle - m_laserAngle);
+    //    if (Timer < collapseTime)
+    //    {
+    //        attackLine.SetPosition(0, Vector2.Lerp(transform.position, _hitPoint, Timer * 2.0f / collapseTime));
+    //    }
+    //}
 
-            return;
-        }
+    //private void ChangeStateTeleport()
+    //{
+    //    _state = State.Teleport;
+    //    _animator.SetTrigger("Teleport");
+    //}
 
-        m_timer += Time.deltaTime;
+    //private void StartAppear()
+    //{
+    //    _state = State.Teleport;
+    //    _animator.SetTrigger("Appear");
+    //    mainSprite.forceRenderingOff = false;
+    //}
 
-        bubbleTransform.localScale = Vector2.Lerp(new Vector2(maxBubbleSize, maxBubbleSize), Vector2.zero, m_timer / collapseTime);
-        attackLine.SetPosition(1, Vector2.Lerp(transform.position, m_hitPoint, m_timer / collapseTime));
+    //private void TeleportWaitState()
+    //{
+    //    if (Timer >= _teleportWaitTime)
+    //    {
+    //        Timer = 0.0f;
 
-    }
+    //        if (_numTeleports >= maxTeleportsBeforeSpike || Random.value < spikeChance)
+    //        {
+    //            _numTeleports = 0;
+    //            ChangeStateSpiking();
+    //        }
+    //        else
+    //        {
+    //            _numTeleports++;
+    //            StartAppear();
+    //        }
 
-    private void ShootState()
-    {
-        if (m_timer >= shootTime)
-        {
-            m_timer = 0.0f;
-            m_state = State.Recover;
-            return;
-        }
+    //        return;
+    //    }
 
-        m_timer += Time.deltaTime;
+    //    Timer += Time.deltaTime;
+    //}
 
-        m_laserAngle += m_laserTraceDir * Time.deltaTime * laserSpeed;
-        Vector2 dir = new Vector2();
-        dir.x = Mathf.Cos(m_laserAngle);
-        dir.y = Mathf.Sin(m_laserAngle);
+    //private void DoTeleport()
+    //{
+    //    mainSprite.forceRenderingOff = true;
+    //    transform.position = FindTeleportLocation();
+    //    _state = State.TeleportWaiting;
 
-        Vector2? endpt = CalcLaserEnd(dir);
-        if (endpt != null)
-        {
-            attackLine.SetPosition(1, (Vector3)endpt);
-            m_hitPoint = endpt.Value;
-        }
-    }
+    //    _teleportWaitTime = Random.Range(teleportWaitTimeRange.x, teleportWaitTimeRange.y);
+    //}
 
-    private void RecoverState()
-    {
-        if (m_timer >= recoverTime)
-        {
-            m_timer = 0.0f;
-            bubbleTransform.localScale = Vector2.zero;
-            attackLine.SetPosition(1, m_hitPoint);
-            attackLine.enabled = false;
-            
-            ChangeStateTeleport();
-            
-            return;
-        }
+    //private void EndTeleport()
+    //{
+    //    ChangeStateCharge();
+    //}
 
-        m_timer += Time.deltaTime;
+    //private void ChangeStateSpiking()
+    //{
+    //    _state = State.Spiking;
+    //    spikeGenerator.StartRandomAttack(spikeSpeed);
+    //}
 
-        if(m_timer < collapseTime)
-        {
-            attackLine.SetPosition(0, Vector2.Lerp(transform.position, m_hitPoint, m_timer * 2.0f / collapseTime));
-        }
-    }
-
-    private void ChangeStateTeleport()
-    {
-        m_state = State.Teleport;
-        m_animator.SetTrigger("Teleport");
-    }
-
-    private void StartAppear()
-    {
-        m_state = State.Teleport;
-        m_animator.SetTrigger("Appear");
-        mainSprite.forceRenderingOff = false;
-    }
-
-    private void TeleportWaitState()
-    {
-        if (m_timer >= m_teleportWaitTime)
-        {
-            m_timer = 0.0f;
-
-            if (m_numTeleports >= maxTeleportsBeforeSpike || Random.value < spikeChance)
-            {
-                m_numTeleports = 0;
-                ChangeStateSpiking();
-            }
-            else
-            {
-                m_numTeleports++;
-                StartAppear();
-            }
-
-            return;
-        }
-
-        m_timer += Time.deltaTime;
-    }
-
-    private void DoTeleport()
-    {
-        mainSprite.forceRenderingOff = true;
-        transform.position = FindTeleportLocation();
-        m_state = State.TeleportWaiting;
-
-        m_teleportWaitTime = Random.Range(teleportWaitTimeRange.x, teleportWaitTimeRange.y);
-    }
-
-    private void EndTeleport()
-    {
-        ChangeStateCharge();
-    }
-
-    private void ChangeStateSpiking()
-    {
-        m_state = State.Spiking;
-        spikeGenerator.StartRandomAttack(spikeSpeed);
-    }
-
-    private void FinishSpike()
-    {
-        StartAppear();
-    }
+    //private void FinishSpike()
+    //{
+    //    StartAppear();
+    //}
 
     private Vector2 FindTeleportLocation()
     {
@@ -303,7 +249,7 @@ public class CrystalBoss : MonoBehaviour
         return res;
     }
 
-    private Vector2? CalcLaserEnd(Vector2 direction)
+    public Vector2? CalcLaserEnd(Vector2 direction)
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, ~GameState.Instance.playerLayerMask);
         if (hit)
@@ -320,5 +266,186 @@ public class CrystalBoss : MonoBehaviour
         Handles.DrawWireDisc(transform.position, Vector3.forward, teleportRangeFromPlayer);
         Handles.color = Color.cyan;
         Handles.DrawWireDisc(transform.position, Vector3.forward, teleportRangeFromWalls);
+    }
+}
+
+public class CrystalBossState
+{
+    protected CrystalBoss _machine { get; private set; }
+
+    public CrystalBossState(CrystalBoss machine) => _machine = machine;
+
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void Update(float delta) { }
+}
+
+public class CrystalBossIdle : CrystalBossState
+{
+    private float _stateTime;
+    public CrystalBossIdle(CrystalBoss state, float stateTime) : base(state)
+    {
+        _stateTime = stateTime;
+    }
+
+    public override void Exit()
+    {
+        _machine.ResetTimer();
+    }
+
+    public override void Update(float delta)
+    {
+        if (_machine.CheckTimer(_stateTime))
+        {
+            _machine.CurrentState = _machine.StateCharge;
+            return;
+        }
+
+        _machine.TickTimer(delta);
+    }
+}
+
+public class CrystalBossCharge : CrystalBossState
+{
+    private float _stateTime;
+    private Transform _bubbleTransform;
+    private float _maxBubbleSize;
+    public CrystalBossCharge(CrystalBoss state, float stateTime, Transform bubbleTransform, float maxBubbleSize) : base(state)
+    {
+        _stateTime = stateTime;
+        _bubbleTransform = bubbleTransform;
+        _maxBubbleSize = maxBubbleSize;
+    }
+
+    public override void Enter()
+    {
+        _machine.targetLine.enabled = true;
+        _machine.targetLine.SetPositions(new Vector3[] { _machine.transform.position, _machine.transform.position });
+    }
+
+    public override void Exit()
+    {
+        _machine.ResetTimer();
+        _bubbleTransform.localScale = new Vector2(_maxBubbleSize, _maxBubbleSize);
+        //_laserAngle = Vector2.SignedAngle(Vector2.right, dir2player) * Mathf.Deg2Rad;
+    }
+
+    public override void Update(float delta)
+    {
+        Vector2 dir2player = (GameState.Instance.player.transform.position - _machine.transform.position).normalized;
+
+        if (_machine.CheckTimer(_stateTime))
+        {
+            _machine.CurrentState = _machine.StateHold;
+            return;
+        }
+
+        _machine.TickTimer(delta);
+
+        _bubbleTransform.localScale = Vector2.Lerp(Vector2.zero, new Vector2(_maxBubbleSize, _maxBubbleSize), _machine.Timer / _stateTime);
+
+        Vector2? endpt = _machine.CalcLaserEnd(dir2player);
+
+        if (endpt != null)
+        {
+            _machine.targetLine.SetPosition(1, endpt.Value);
+            //_hitPoint = endpt.Value;
+        }
+    }
+}
+
+public class CrystalBossHold : CrystalBossState
+{
+    private float _stateTime;
+    private Transform _bubbleTransform;
+    private float _maxBubbleSize;
+    private float _holdWobble;
+    private float _holdAmplitude;
+
+    public CrystalBossHold(CrystalBoss state, float stateTime, Transform bubbleTransform, float holdWobble, float holdAmplitude) : base(state)
+    {
+        _stateTime = stateTime;
+        _bubbleTransform = bubbleTransform;
+        _maxBubbleSize = 0.0f;
+        _holdWobble = holdWobble;
+        _holdAmplitude = holdAmplitude;
+    }
+
+    public override void Enter()
+    {
+        _maxBubbleSize = _bubbleTransform.localScale.x;
+        _machine.targetLine.enabled = true;
+    }
+
+    public override void Exit()
+    {
+        _machine.ResetTimer();
+        _machine.targetLine.enabled = false;
+        //_laserAngle = Vector2.SignedAngle(Vector2.right, dir2player) * Mathf.Deg2Rad;
+    }
+
+    public override void Update(float delta)
+    {
+
+        if(_machine.CheckTimer(_stateTime))
+        {
+            _machine.CurrentState = _machine.StateCollapse;
+            return;
+        }
+        _machine.TickTimer(delta);
+
+        float s = Mathf.Sin(Time.time * _holdWobble) * _holdAmplitude;
+        _bubbleTransform.localScale = new Vector2(_maxBubbleSize + s, _maxBubbleSize + s);
+    }
+}
+
+public class CrystalBossCollapse : CrystalBossState
+{
+    private float _stateTime;
+    private Transform _bubbleTransform;
+    private float _maxBubbleSize;
+
+    public CrystalBossCollapse(CrystalBoss state, float stateTime, Transform bubbleTransform) : base(state)
+    {
+        _stateTime = stateTime;
+        _bubbleTransform = bubbleTransform;
+        _maxBubbleSize = 0.0f;
+    }
+
+    public override void Enter()
+    {
+        _machine.attackLine.enabled = true;
+        _machine.attackLine.SetPosition(0, _machine.transform.position);
+        _machine.attackLine.SetPosition(1, _machine.transform.position);
+
+        _maxBubbleSize = _bubbleTransform.localScale.x;
+    }
+
+    public override void Exit()
+    {
+        _machine.ResetTimer();
+        //_laserAngle = Vector2.SignedAngle(Vector2.right, dir2player) * Mathf.Deg2Rad;
+    }
+
+    public override void Update(float delta)
+    {
+        if (_machine.CheckTimer(_stateTime))
+        {
+            _machine.CurrentState = _machine.StateIdle;
+            //_state = State.Shoot;
+
+            _bubbleTransform.localScale = Vector2.zero;
+            _machine.attackLine.SetPosition(1, _machine.targetLine.GetPosition(1));
+
+            //Vector2 dir2player = (GameState.Instance.player.transform.position - transform.position).normalized;
+            //float playerAngle = Vector2.SignedAngle(Vector2.right, dir2player) * Mathf.Deg2Rad;
+            //_laserTraceDir = Mathf.Sign(playerAngle - _laserAngle);
+
+            return;
+        }
+        _machine.TickTimer(delta);
+
+        _bubbleTransform.localScale = Vector2.Lerp(new Vector2(_maxBubbleSize, _maxBubbleSize), Vector2.zero, _machine.Timer / _stateTime);
+        _machine.attackLine.SetPosition(1, Vector2.Lerp(_machine.transform.position, _machine.targetLine.GetPosition(1), _machine.Timer / _stateTime));
     }
 }
